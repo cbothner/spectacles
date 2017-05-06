e6d4b5d61f12a44b2abaf25cd3fbc2da6c9cab0f
Cameron Bothner <cameronbothner@gmail.com>
2017-05-06 19:59:41 -0400

    Fixes PUT request format

--- src/api.js
+++ src/api.js
@@ -1,68 +1,58 @@
 export default {
-
   get(endpoint) {
-    let r = new Request(
-      endpoint, {
-        credentials: 'same-origin',
-        method: 'GET',
-        headers: new Headers({
-          'Accept': 'application/json',
-        })
-      }
-    )
-    return fetch(r).then(validate)
+    let r = new Request(endpoint, {
+      credentials: 'same-origin',
+      method: 'GET',
+      headers: new Headers({
+        Accept: 'application/json',
+      }),
+    });
+    return fetch(r).then(validate);
   },
 
   post(endpoint, params) {
-    let body = JSON.stringify(params)
-    let r = new Request(
-      endpoint, {
-        credentials: 'same-origin',
-        method: 'POST',
-        body: body,
-        headers: new Headers({
-          'Accept': 'application/json',
-          'Content-Type': 'application/json'
-        })
-      }
-    )
-    return fetch(r).then(validate)
+    let body = JSON.stringify(params);
+    let r = new Request(endpoint, {
+      credentials: 'same-origin',
+      method: 'POST',
+      body: body,
+      headers: new Headers({
+        Accept: 'application/json',
+        'Content-Type': 'application/json',
+      }),
+    });
+    return fetch(r).then(validate);
   },
 
-  patch(endpoint, params) {
-    let body = JSON.stringify(params)
-    let r = new Request(
-      endpoint, {
-        credentials: 'same-origin',
-        method: 'PATCH',
-        body: body,
-        headers: new Headers({
-          'Accept': 'application/json',
-          'Content-Type': 'application/json'
-        })
-      }
-    )
-    return fetch(r).then(validate)
+  put(endpoint, params) {
+    let body = JSON.stringify(params);
+    let r = new Request(endpoint, {
+      credentials: 'same-origin',
+      method: 'PUT',
+      body: body,
+      headers: new Headers({
+        Accept: 'application/json',
+        'Content-Type': 'application/json',
+      }),
+    });
+    return fetch(r).then(validate);
   },
 
   delete(endpoint) {
-    let r = new Request(
-      endpoint, {
-        credentials: 'same-origin',
-        method: 'DELETE',
-        headers: new Headers({
-          'Accept': 'application/json'
-        })
-      }
-    )
-    return fetch(r)
-  }
+    let r = new Request(endpoint, {
+      credentials: 'same-origin',
+      method: 'DELETE',
+      headers: new Headers({
+        Accept: 'application/json',
+      }),
+    });
+    return fetch(r);
+  },
+};
 
-}
-
-function validate( response ) {
+function validate(response) {
   if (!response.ok) {
-    throw new Error(`Status ${response.status}`)
+    throw new Error(`Status ${response.status}`);
   }
-  return response.json()
+  return response.json();
 }

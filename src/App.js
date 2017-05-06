43410bd2e6acf163d3a58d86f2a3d719cfd8b7c3
Cameron Bothner <cameronbothner@gmail.com>
2017-05-06 19:09:49 -0400

    Prepares for deployment on Heroku

--- src/App.js
+++ src/App.js
@@ -1,35 +1,37 @@
-import React, { Component } from 'react'
+import React, {Component} from 'react';
 
-import { createStore, applyMiddleware } from 'redux'
-import { Provider } from 'react-redux'
-import thunk from 'redux-thunk'
+import {createStore, applyMiddleware} from 'redux';
+import {Provider} from 'react-redux';
+import thunk from 'redux-thunk';
 
-import { BrowserRouter as Router, Route } from 'react-router-dom'
+import {HashRouter as Router, Route} from 'react-router-dom';
 
-import "@blueprintjs/core/dist/blueprint.css"
-import './App.css'
+import '@blueprintjs/core/dist/blueprint.css';
+import './App.css';
 
-import { FocusStyleManager  } from "@blueprintjs/core";
+import {FocusStyleManager} from '@blueprintjs/core';
 FocusStyleManager.onlyShowFocusOnTabs();
 
-import reducer from './reducers.js'
-import { getFilters, getSchedules } from './actions.js'
+import reducer from './reducers.js';
+import {getFilters, getSchedules} from './actions.js';
 
-import Spectacles from './components/spectacles.js'
+import Spectacles from './components/spectacles.js';
 
-const store = createStore( reducer, applyMiddleware(thunk) )
+const store = createStore(reducer, applyMiddleware(thunk));
 
-store.dispatch(getFilters())
-store.dispatch(getSchedules())
+store.dispatch(getFilters());
+store.dispatch(getSchedules());
 
 class App extends Component {
   render() {
-    return <Provider store={store}>
-      <Router>
-        <Route path="/" component={Spectacles} />
-      </Router>
-    </Provider>
+    return (
+      <Provider store={store}>
+        <Router>
+          <Route path="/" component={Spectacles} />
+        </Router>
+      </Provider>
+    );
   }
 }
 
-export default App
+export default App;

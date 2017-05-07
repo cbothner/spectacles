110fe53303d502cd59d597cfe966d720b9080135
Cameron Bothner <cameronbothner@gmail.com>
2017-05-07 11:15:51 -0400

    Rounds SpectrophotometerData to two decimal points

--- src/components/spectrophotometer_data.js
+++ src/components/spectrophotometer_data.js
@@ -1,56 +1,84 @@
-import React from 'react'
-import { connect } from 'react-redux'
+import React from 'react';
+import {connect} from 'react-redux';
 
-import { NonIdealState } from '@blueprintjs/core'
+import {NonIdealState} from '@blueprintjs/core';
 
-import SpectrophotometerChart from './spectrophotometer_chart.js'
-import { updateFilter } from '../actions.js'
+import SpectrophotometerChart from './spectrophotometer_chart.js';
+import {updateFilter} from '../actions.js';
 
 const mapStateToProps = (state, ownProps) => {
   return {
     data: state.filtersById[ownProps.id].spectrophotometerData,
-  }
-}
+  };
+};
 
 const mapDispatchToProps = (dispatch, ownProps) => {
   return {
-    onChange: (e) => {
-      dispatch(updateFilter(ownProps.id, {
-        spectrophotometerData: convertCSVToData(e.currentTarget.value)
-      }))
-    }
-  }
-}
+    onChange: e => {
+      dispatch(
+        updateFilter(ownProps.id, {
+          spectrophotometerData: convertCSVToData(e.currentTarget.value),
+        }),
+      );
+    },
+  };
+};
 
-const SpectrophotometerData = ({data, onChange}) =>
+const SpectrophotometerData = ({data, onChange}) => (
   <div className="pt-card" style={{marginTop: '1em'}}>
     <h5>Spectrophotometer Data</h5>
     <div style={{display: 'flex'}}>
-      <textarea style={{flex: 1, height: 200}} value={convertDataToCSV(data)} onChange={onChange} />
-      <div style={{flex: 3, display: 'flex', flexDirection: 'column', justifyContent: 'center'}}>
-        { data
+      <textarea
+        style={{flex: 1, height: 200}}
+        value={convertDataToCSV(data)}
+        onChange={onChange}
+      />
+      <div
+        style={{
+          flex: 3,
+          display: 'flex',
+          flexDirection: 'column',
+          justifyContent: 'center',
+        }}
+      >
+        {data
           ? <SpectrophotometerChart data={data} />
           : <NonIdealState
               title="No Data"
               description="Paste wavelength, OD, and %T data from Excel."
-              visual="timeline-line-chart" />
-        }
+              visual="timeline-line-chart"
+            />}
       </div>
     </div>
   </div>
+);
 
-export default connect(mapStateToProps, mapDispatchToProps)(SpectrophotometerData)
-
+export default connect(mapStateToProps, mapDispatchToProps)(
+  SpectrophotometerData,
+);
 
 function convertDataToCSV(data = []) {
-  return data.map((row) => `${row.wavelength},${row.od},${row.transmittance}`).join('\n')
+  return data
+    .map(row => `${row.wavelength},${row.od},${row.transmittance}`)
+    .join('\n');
 }
 
 function convertCSVToData(csv) {
-  return csv.split('\n').map((row) => {
-    let x = row.split(/[, \t]+/)
-    return x.length === 3 && !isNaN(x[0])
-      ? { wavelength: parseInt(x[0], 10), od: parseFloat(x[1]), transmittance: parseFloat(x[2]) }
-      : null
-  }).filter(x => x)
+  return csv
+    .split('\n')
+    .map(row => {
+      let x = row.split(/[, \t]+/);
+      return x.length === 3 && !isNaN(x[0])
+        ? {
+            wavelength: parseInt(x[0], 10),
+            od: round(x[1]),
+            transmittance: round(x[2]),
+          }
+        : null;
+    })
+    .filter(x => x);
+}
+
+function round(x) {
+  return Math.round(parseFloat(x) * 100) / 100;
 }

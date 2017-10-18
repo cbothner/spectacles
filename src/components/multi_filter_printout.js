778e15a20066e4758ba72db68d4193ae19aff2a9
Cameron Bothner <cameronbothner@gmail.com>
2017-10-18 13:13:35 -0400

    Fixes top spacing on subsequent pages of multifilter printout

--- src/components/multi_filter_printout.js
+++ src/components/multi_filter_printout.js
@@ -11,70 +11,73 @@ const MultiFilterPrintout = ({ suggestions, filtersById }) => {
       {suggestions.map(suggestion => {
         const filter = filtersById[suggestion.filterId]
         return (
-          <div key={filter.id} className="pt-card" style={{ margin: '1em 0' }}>
-            <div style={{ display: 'flex', justifyContent: 'space-between' }}>
-              <div>
-                <p>
-                  <strong style={{ fontSize: 20, marginRight: '0.5em' }}>
-                    {filter.name}
-                  </strong>
-                  <span
-                    className="pt-text-muted"
-                    style={{
-                      textDecoration: suggestion.specialPrice
-                        ? 'line-through'
-                        : 'none'
-                    }}
-                  >
-                    ${filter.basePrice} per pair
-                  </span>{' '}
-                  {suggestion.specialPrice &&
-                    <span style={{ color: Colors.GREEN1 }}>
-                      Your price: ${suggestion.specialPrice}
-                    </span>}
-                </p>
+          <div style={{ padding: '1em 0 0' }}>
+            <div key={filter.id} className="pt-card">
+              <div style={{ display: 'flex', justifyContent: 'space-between' }}>
+                <div>
+                  <p>
+                    <strong style={{ fontSize: 20, marginRight: '0.5em' }}>
+                      {filter.name}
+                    </strong>
+                    <span
+                      className="pt-text-muted"
+                      style={{
+                        textDecoration: suggestion.specialPrice
+                          ? 'line-through'
+                          : 'none'
+                      }}
+                    >
+                      ${filter.basePrice} per pair
+                    </span>{' '}
+                    {suggestion.specialPrice && (
+                      <span style={{ color: Colors.GREEN1 }}>
+                        Your price: ${suggestion.specialPrice}
+                      </span>
+                    )}
+                  </p>
+                </div>
+                <div style={{ textAlign: 'right' }}>
+                  <p>
+                    EN207 {filter.ce ? 'Certified' : 'Pending'}
+                    <br />
+                    Luminous transmittance: {filter.vlt}% {filter.color}
+                  </p>
+                </div>
               </div>
-              <div style={{ textAlign: 'right' }}>
-                <p>
-                  EN207 {filter.ce ? 'Certified' : 'Pending'}
-                  <br />
-                  Luminous transmittance: {filter.vlt}% {filter.color}
-                </p>
-              </div>
-            </div>
 
-            <div style={{ display: 'flex', alignItems: 'flex-start' }}>
-              <RangeTablePrintout
-                title="CE Rating"
-                name="L-Ratings"
-                items={filter.lRatings}
-                first
-                Header="h5"
-              />
-              <RangeTablePrintout
-                title="Optical Density"
-                name="ODs"
-                items={filter.ods}
-                Header="h5"
-              />
-              <div
-                style={{
-                  flex: 1,
-                  display: 'flex',
-                  flexDirection: 'column',
-                  justifyContent: 'center',
-                  marginLeft: '1em',
-                  marginTop: '-1em',
-                  marginBottom: '-1em'
-                }}
-              >
-                <SpectrophotometerChart
-                  data={filter.spectrophotometerData}
-                  forPrint
-                  center
-                  width={290}
-                  height={140}
+              <div style={{ display: 'flex', alignItems: 'flex-start' }}>
+                <RangeTablePrintout
+                  title="CE Rating"
+                  name="L-Ratings"
+                  items={filter.lRatings}
+                  first
+                  Header="h5"
                 />
+                <RangeTablePrintout
+                  title="Optical Density"
+                  name="ODs"
+                  items={filter.ods}
+                  Header="h5"
+                />
+                <div
+                  style={{
+                    flex: 1,
+                    display: 'flex',
+                    flexDirection: 'column',
+                    justifyContent: 'center',
+                    marginLeft: '1em',
+                    marginTop: '-1em',
+                    marginBottom: '-1em'
+                  }}
+                >
+                  <SpectrophotometerChart
+                    data={filter.spectrophotometerData}
+                    forPrint
+                    center
+                    width={290}
+                    height={140}
+                  />
+                </div>
               </div>
             </div>
           </div>
